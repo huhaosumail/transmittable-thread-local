d5087052c4e7df56d179116381fafda2050c62ac
huhao80 <huhao80@jd.com>
2023-06-20 14:42:03 +0800

    question

--- ttl2-compatible/src/test/java/com/alibaba/demo/TTlWithNoStandardDemo.java
+++ ttl2-compatible/src/test/java/com/alibaba/demo/TTlWithNoStandardDemo.java
@@ -41,7 +41,7 @@ public class TTlWithNoStandardDemo {
             //所以包装后 导致父线程传递到子线程 子线程也会为空 但是不代表子线程真的为空  子线程执行完任务后
             //会重新覆盖原来的子线程的值  但是这里看不出来 只是认为没有值了
             threadPoolExecutor.submit(TtlRunnable.get(runnable2));
-            //这里执行异步任务 获取有值  明明在上面主线程remove了  这里两步不一样的原因是什么呢？
+            //这里执行异步任务 获取有值  明明在上面主线程remove了  是不是子线程也同步了上下文 那是不是就存在内存泄漏呢？
             threadPoolExecutor.submit(runnable3);
         }
 
